b8e58e6177674113324aa5ddf887da7e4cb386aa
LaurentJeannel <69810104+LaurentJeannel@users.noreply.github.com>
2023-03-29 19:13:29 +0200

    n
    n

--- smartlife2.js
+++ smartlife2.js
@@ -10,8 +10,7 @@ var m='async function asyncCall'
 for(i = 0; i < longueur; i++) {
        eval( k + i + ' = '+ "new TuyAPI({id: objet["+i+"]['id'],key: objet["+i+"]['key']})");
       ff=objet[i]["name"]
-      //eval ( m + i + '(){await device'+eval(i)+'.find(); await device'+eval(i)+'.connect();console.log("'+ff+'");let status = await device'+eval(i)+'.get(); console.log(`Current status: ${status}.`);} ')
-eval ( m + i + '(){await device'+eval(i)+'.find(); await device'+eval(i)+'.connect()} ')
+      eval ( m + i + '(){await device'+eval(i)+'.find(); await device'+eval(i)+'.connect()} ')
 
 }
 
@@ -34,7 +33,7 @@ console.log(phrasedomo,' reco ')
 for(var i=0;i<objet.length;i++){
     var t=objet[i]['name']
     t=t.trim()
-    if(phrasedomo.search(new RegExp(t,"gi"))>-1){var result=i}
+    if(phrasedomo.search(new RegExp(t,"gi"))>-1){var result=i;console.log("result = "+i)}
 }
 
 
@@ -43,32 +42,27 @@ if(result ===undefined){console.log('rien'); return false}
 
 
 if(data.verifdomo=="vérif"){
-   
-        //var m="device";
-        eval(m+result+".get({schema: true}).then(dataa => {console.log(dataa.dps['1']) ;console.log(dataa.dps['20']); console.log(dataa);if(dataa.dps['1']==false){JarvisIASpeech('objet off')};if(dataa.dps['1']==true){JarvisIASpeech('objet on')};if(dataa.dps['20']==false){JarvisIASpeech('objet off')};if(dataa.dps['20']==true){JarvisIASpeech('objet on')};} )");
-    
+    eval(m+result+".get({schema: true}).then(dataa => {console.log(dataa.dps['1']) ;console.log(dataa.dps['20']); console.log(dataa);if(dataa.dps['1']==false){JarvisIASpeech('objet off')};if(dataa.dps['1']==true){JarvisIASpeech('objet on')};if(dataa.dps['20']==false){JarvisIASpeech('objet off')};if(dataa.dps['20']==true){JarvisIASpeech('objet on')};} )");
     return false
 }
 
-//JarvisIASpeech(${data.dps['1']})
-         //device.on('data', data => {console.log('Data from device:', data);})
-
 (async () => {
+    var dataDPSonoff="0"
     var m="device"
-eval(m+result+".get({schema: true}).then(datab => {console.log(datab)})");
-  //; if(dataa.dps['1']==false){JarvisIASpeech('objet off')}; if(dataa.dps['1']==true){JarvisIASpeech('objet on')} })");
-    //eval(m+result+".on('data', data => {console.log('Data from device:', data.dps);})")
-    //eval(m+result+".on('dp-refresh', data => {console.log('DP_REFRESH data from device: ', data);})")
+await eval(m+result+".get({schema: true}).then(datab => {datadomo=datab;console.log(datab)})");
+if(datadomo.dps[1]!==undefined){dataDPSonoff="1"}else{dataDPSonoff="20"}
 
-    if(phrasedomo.search(new RegExp("vert","gi"))>-1){eval( m+result+'.set({dps: 1, set: true})');eval(m+result+'.set({dps: 5, set: "23ff00006fffff"})');}
-    if(phrasedomo.search(new RegExp("rouge","gi"))>-1){eval( m+result+'.set({dps: 1, set: true})');eval(m+result+'.set({dps: 5, set: "ff01000000ffff"})');}
-    if(phrasedomo.search(new RegExp("bleu","gi"))>-1){eval( m+result+'.set({dps: 1, set: true})');eval(m+result+'.set({dps: 5, set: "01001c00f3fe19"})');}
-    if(data.verifdomo=="on"){eval(m+result+'.set({dps: 1, set: true})');eval(m+result+'.set({dps: 20, set: true})')}
-    if(data.verifdomo=="off"){eval(m+result+'.set({dps: 1, set: false})');eval(m+result+'.set({dps: 20, set: false})')}  
-    if(data.verifdomo=="disconect"){ device.disconnect();}
-    //eval(m+result+".get({schema: true}).then(dataa => console.log(dataa))")
-    //device8.on('dp-refresh', data => {console.log('DP_REFRESH data from device: ', data);})  
-   // device8.on('data', data => {console.log('Data from device:222', data);})
+console.log("valeur on off : "+dataDPSonoff)
+
+  
+    if(phrasedomo.search(new RegExp("vert","gi"))>-1){eval( m+result+'.set({multiple: true,data: {dataDPSonoff: true,"2" : "colour", "5": "23ff00006fffff"}}).then(() => console.log("device was changed"))');}
+    if(phrasedomo.search(new RegExp("rouge","gi"))>-1){eval( m+result+'.set({multiple: true,data: {dataDPSonoff: true,"2" : "colour", "5": "ff01000000ffff"}}).then(() => console.log("device was changed"))');}
+    if(phrasedomo.search(new RegExp("bleu","gi"))>-1){eval( m+result+'.set({multiple: true,data: {dataDPSonoff: true,"2" : "colour", "5": "01001c00f3fe19"}}).then(() => console.log("device was changed"))');}
+    if(phrasedomo.search(new RegExp("blanc","gi"))>-1){eval( m+result+'.set({multiple: true,data: {dataDPSonoff: true,"2" : "white"}}).then(() => console.log("device was changed"))');}
+    if(data.verifdomo=="on"){eval(m+result+'.set({dps: dataDPSonoff, set: true})')}
+    if(data.verifdomo=="off"){eval(m+result+'.set({dps: dataDPSonoff, set: false})')}  
+    //if(data.verifdomo=="disconect"){eval(m+result+'.device.disconnect()')}
+   
 })();
 
 }catch(err){console.log(err);return false}
